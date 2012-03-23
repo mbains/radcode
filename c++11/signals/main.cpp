970eda0abf92ac5d7161e7faa5285a1baed5cf1f
Manjinder <manjinder.bains@gmail.com>
2012-03-22 20:43:03 -0700

    closures

--- c++11/signals/main.cpp
+++ c++11/signals/main.cpp
@@ -12,37 +12,45 @@ void test(int a, int b) {
     cout << "test function: " << a << " " << b << "\n";
 }
 
-class Fun {
+class Klass {
 
 public:
     int id;
     void run(int a, int b) {
-        cout << "Fun, ID: " << id << " - run " << a << " " << b << "\n";
+        cout << "Klass, ID: " << id << " - run " << a << " " << b << "\n";
+    }
+    void runWithExtraArgs(string str, int a, int b) {
+
+        cout << "Klass, ID: " << id << " - runWithExtraArgs " << str << " " << a << " " << b << "\n";
     }
 };
 
 int main(int argc, char *argv[])
 {
    
-    Fun f = {id: 5};
-    signals::Signal<int, int> onEvent;
+    Klass f = {id: 5};
+    signals::Signal<int, int> event1;
     signals::Signal<int, int> event2;
 
-    onEvent.bind([&](int a, int b) {
+    event1.bind([&](int a, int b) {
         std::cout << "Event triggered " << a << ", " << b << "\n";
     });
     
 
-    auto  handler = std::bind(&Fun::run, f, std::placeholders::_1, std::placeholders::_2);
-    onEvent.bind(handler);
+    auto  handler = bind(&Klass::run, f, placeholders::_1, placeholders::_2);
+    event1.bind(handler);
 
-    onEvent.bind(test);
+    event1.bind(test);
 
-    Fun f2 = {id: 10};
-    event2.bind( std::bind(&Fun::run, f2, std::placeholders::_1, std::placeholders::_2));
+    Klass f2 = {id: 10};
+    event2.bind( bind(&Klass::run, f2, placeholders::_1, placeholders::_2));
 
     event2.trigger(20, 30);
     
-    onEvent.trigger(10, 20);
+    event1.trigger(10, 20);
+
+    event2.bind( bind(&Klass::runWithExtraArgs, f, "Stuff", placeholders::_1, placeholders::_2));
+
+    event2.trigger(100, 200);
 
 }
