2dd37b1358e96ca3a1776c3b219b166ba5a05e04
Manjinder <manjinder.bains@gmail.com>
2012-03-22 20:17:58 -0700

    class member to functor

--- c++11/signals/main.cpp
+++ c++11/signals/main.cpp
@@ -8,32 +8,40 @@
 
 using namespace std;
 
-void test() {
-    cout << "test function\n";
+void test(int a, int b) {
+    cout << "test function: " << a << " " << b << "\n";
 }
 
 class Fun {
+
 public:
+    int id;
     void run(int a, int b) {
-        cout << "fun run " << a << " " << b << "\n";
+        cout << "Fun, ID: " << id << " - run " << a << " " << b << "\n";
     }
 };
 
 int main(int argc, char *argv[])
 {
    
-    Fun f;
+    Fun f = {id: 5};
     signals::Signal<int, int> onEvent;
-    
+    signals::Signal<int, int> event2;
+
     onEvent.bind([&](int a, int b) {
         std::cout << "Event triggered " << a << ", " << b << "\n";
-	test();
-	f.run(a, b);
     });
     
 
-    auto  handler = std::bind(&Fun::run, f);
-    //onEvent.bind(&handler); //this does not work
+    auto  handler = std::bind(&Fun::run, f, std::placeholders::_1, std::placeholders::_2);
+    onEvent.bind(handler);
+
+    onEvent.bind(test);
+
+    Fun f2 = {id: 10};
+    event2.bind( std::bind(&Fun::run, f2, std::placeholders::_1, std::placeholders::_2));
+
+    event2.trigger(20, 30);
     
     onEvent.trigger(10, 20);
 
